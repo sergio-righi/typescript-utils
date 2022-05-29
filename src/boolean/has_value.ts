4681d1d5e595856128201ee9bfe20d8b9f8709df
Sérgio Righi <sergiohautrive@gmail.com>
2022-05-29 17:50:15 -0400

    has_value modified

--- src/boolean/has_value.ts
+++ src/boolean/has_value.ts
@@ -1,10 +1,9 @@
-import { isString } from "./is_string";
 /**
  * It checks if the string has value
  * @param {string} value string to be tested
  * @returns {boolean} true if the value is valid
  */
 
-const hasValue = (value: string): boolean => !!value && isString(value) && value.trim() !== "";
+const hasValue = (value: string): boolean => !!value && value.trim() !== "";
 
 export { hasValue };
